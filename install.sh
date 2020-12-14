f90ef2f6cf560e245f9d643e0b58faa91e9196d6
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:24:25 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:23 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:23 von Leander Jedamus
+
+cp -Rvp active-print.py locale $HOME/bin
+
+# vim:ai sw=2
+
